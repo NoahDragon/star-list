636c09562849ea2244ad72fc8093f47ec26a0537
Abner Chou <hi@abnerchou.me>
2019-10-21 01:20:20 -0400

    Starter

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <starlist 
+      :stars="stars" 
+      @edit:star="editSTAR"
+      />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import starlist from './components/StarList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    starlist,
+  },
+  data() {
+    return {
+      stars:[
+        {
+          id: 1,
+          s: "situation",
+          t: "task",
+          a: "action",
+          r: "results",
+        },
+        {
+          id: 2,
+          s: "test s",
+          t: "test t",
+          a: "test a",
+          r: "test r",
+        },
+      ],
+    }
+  },
+  methods: {
+    editSTAR(id, updatedSTAR) {
+      this.stars = this.stars.map(star => star.id === id ? updatedSTAR : star);
+    }
+  },
 }
 </script>
 
